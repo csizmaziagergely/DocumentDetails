8e46c914a9ccd7734b44e5964aba69173ea4cc4d
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-12 23:14:41 +0200

    new: creating the basic context

---
+++ DocumentDetails/DocumentDetailsContext.cs
@@ -0,0 +1,27 @@
+﻿using DocumentDetails.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DocumentDetails
+{
+    public class DocumentDetailsContext : DbContext
+    {
+        public DocumentDetailsContext(DbContextOptions<DocumentDetailsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<DocumentLog> DocumentLogs { get; set; }
+        public DbSet<Event> EventLogs { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Document>().ToTable("dokumentumok");
+            modelBuilder.Entity<DocumentLog>().HasNoKey().ToTable("naplo");
+            modelBuilder.Entity<Event>().ToTable("esemeny");
+        }
+    }
+}
