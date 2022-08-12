d028272ece0055a51a05168fb05fff8c9df69758
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-12 21:28:31 +0200

    feat: creating entities

---
+++ DocumentDetails/Entities/Document.cs
@@ -0,0 +1,14 @@
+﻿using DocumentDetails.Enums;
+
+namespace DocumentDetails.Entities
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Extension Extension { get; set; }
+        public Document? Parent { get; set; }
+        public int? ParentId { get; set; }
+        public Source Source { get; set; }
+    }
+}
---
+++ DocumentDetails/Entities/DocumentLog.cs
@@ -0,0 +1,12 @@
+﻿namespace DocumentDetails.Entities
+{
+    public class DocumentLog
+    {
+        public int DocumentId { get; set; }
+        public Document Document { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+        public DateTime HappenedAt { get; set; }
+
+    }
+}
