41e466b3c9e6e7edbe575b413ba311656b0a9854
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-13 19:52:45 +0200

    add: document event DTO to list events

---
+++ DocumentDetails/DTOs/DocumentEvent.cs
@@ -0,0 +1,12 @@
+﻿using CsvHelper.Configuration;
+using DocumentDetails.Entities;
+
+namespace DocumentDetails.DTOs
+{
+    public class DocumentEvent
+    {
+        public string Title { get; set; }
+        public string HappenedAt { get; set; }
+
+    }
+}
