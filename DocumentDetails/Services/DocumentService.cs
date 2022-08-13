9cff5653e01c0055bf16255a5c091e38c1a46b9e
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-13 17:24:28 +0200

    add: DocumentService with GetDocuments

---
+++ DocumentDetails/Services/DocumentService.cs
@@ -0,0 +1,20 @@
+﻿using DocumentDetails.Entities;
+using DocumentDetails.Repositories;
+
+namespace DocumentDetails.Services
+{
+    public class DocumentService
+    {
+        private readonly DocumentRepository _documentRepository;
+
+        public DocumentService(DocumentRepository documentRepository)
+        {
+            _documentRepository = documentRepository;
+        }
+
+        public async Task<List<Document>> GetDocuments()
+        {
+            return (await _documentRepository.GetAll());
+        }
+    }
+}
