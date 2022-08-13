69f4fccbc30d22cf8bcbd16ea31a329ecb1ca71e
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-13 17:24:02 +0200

    add: DocumentRepository with GetAll

---
+++ DocumentDetails/Repositories/DocumentRepository.cs
@@ -0,0 +1,23 @@
+﻿using DocumentDetails.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentDetails.Repositories
+{
+    public class DocumentRepository
+    {
+        private readonly DocumentDetailsContext _context;
+
+        public DocumentRepository(DocumentDetailsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Document>> GetAll()
+        {
+            return await _context.Documents
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+    }
+}
