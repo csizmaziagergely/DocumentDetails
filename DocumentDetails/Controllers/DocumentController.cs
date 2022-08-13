a4752a6699745919fe045a9c9848496e076cda17
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-13 17:29:18 +0200

    feat: Get all document on api/documents route

--- DocumentDetails/Controllers/DocumentController.cs
+++ DocumentDetails/Controllers/DocumentController.cs
@@ -1,6 +1,25 @@
-﻿namespace DocumentDetails.Controllers
+﻿using DocumentDetails.Entities;
+using DocumentDetails.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentDetails.Controllers
 {
-    public class DocumentController
+    [ApiController]
+    [Route("api/documents")]
+    public class DocumentController : ControllerBase
     {
+        private readonly DocumentService _documentService;
+
+        public DocumentController(DocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Document>>> GetAll()
+        {
+            var documents = await _documentService.GetDocuments();
+            return documents;
+        }
     }
 }
