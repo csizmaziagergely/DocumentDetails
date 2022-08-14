53611b1d4615c12e8f8009ad2b7fc497c6a19f19
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-14 20:29:16 +0200

    add: CORS policy to connect with frontend

--- DocumentDetails/Program.cs
+++ DocumentDetails/Program.cs
@@ -3,9 +3,21 @@ using DocumentDetails.Repositories;
 using DocumentDetails.Services;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DocumentDetailsContext>(options =>
@@ -18,6 +30,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
