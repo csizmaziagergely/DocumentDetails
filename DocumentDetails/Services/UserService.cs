5e29141ba673f688910ea70d63c3a2b1f77583e9
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 11:39:35 +0200

    add: user service to register user

---
+++ DocumentDetails/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using BpRobotics.Core.Extensions;
+using DocumentDetails.DTOs;
+using DocumentDetails.Repositories;
+
+namespace DocumentDetails.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<UserView> NewUser(UserCreate newUser)
+        {
+            var userEntity = newUser.ToUserEntity();
+            await _userRepository.Add(userEntity);
+            return userEntity.ToUserView();
+        }
+
+        public async Task<UserView> GetById(int userId) => (await _userRepository.GetById(userId)).ToUserView();
+
+    }
+}
