80084c7149bee855e37cbec4707c878c33035c00
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 11:38:38 +0200

    add: password hasher

---
+++ DocumentDetails/Util/BCryptPasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿namespace DocumentDetails.Util
+{
+    public class BCryptPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
