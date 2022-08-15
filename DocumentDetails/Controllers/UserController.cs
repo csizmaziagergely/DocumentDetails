7583496b8b59bb1a5225fc90ce6e01e589b07ead
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 11:40:04 +0200

    feature: adding user with api controller

---
+++ DocumentDetails/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using DocumentDetails.DTOs;
+using DocumentDetails.Services;
+using DocumentDetails.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentDetails.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        private readonly BCryptPasswordHasher _hasher;
+
+        public UserController(UserService userService, BCryptPasswordHasher hasher)
+        {
+            _userService = userService;
+            _hasher = hasher;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserView>> NewUser(UserCreate newUser)
+        {
+            newUser.Password = _hasher.HashPassword(newUser.Password);
+            try
+            {
+                var createdUser = await _userService.NewUser(newUser);
+                return CreatedAtRoute("GetUserById", new { userId = createdUser.Id }, createdUser);
+            }
+            catch (DbUpdateException e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{userId}", Name = "GetUserById")]
+        public async Task<ActionResult<UserView>> GetUserById(int userId)
+        {
+            try
+            {
+                return await _userService.GetById(userId);
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound($"User with ID:{userId} not found.");
+            }
+        }
+    }
+}
