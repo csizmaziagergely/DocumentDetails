2362f213503d02c6856d00f7ac6295f65ffc740a
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 23:28:29 +0200

    Add: request interceptor

--- DocumentDetails/Client/clientapp/src/hooks/useAxios.js
+++ DocumentDetails/Client/clientapp/src/hooks/useAxios.js
@@ -1,13 +1,20 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "../fetch/axiosInstance";
+import useAuth from "./useAuth";
+
 
 function useAxios() {
+    const { auth, logout } = useAuth();
+
     const navigate = useNavigate();
 
     useEffect(() => {
         const requestIntercept = axios.interceptors.request.use(
             config => {
+                if (!config.headers["Authorization"] && auth?.accessToken) {
+                    config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+                }
                 return config;
             }, error => Promise.reject(error)
         );
@@ -15,6 +22,9 @@ function useAxios() {
             response => {
                 return response;
             }, error => {
+                if (error.response.status === 401) {
+                    navigate("/login");
+                }
                 return Promise.reject(error);
             }
         )
