03ed19734cb6b13a2baee8ae873420a86c647dee
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 18:56:14 +0200

    add: authenticator service generating accesstokens

---
+++ DocumentDetails/Services/Authenticator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using DocumentDetails.Entities;
+using DocumentDetails.DTOs;
+
+namespace DocumentDetails.Services
+{
+    public class Authenticator
+    {
+        private readonly AuthenticationConfiguration _configuration;
+
+        public Authenticator(IOptions<AuthenticationConfiguration> configuration)
+        {
+            _configuration = configuration.Value;
+        }
+
+        public string GenerateToken(string secretKey, string issuer, string audience, double expirationMinutes,
+            IEnumerable<Claim> claims = null)
+        {
+            SecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddMinutes(expirationMinutes),
+                credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateAccessToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim("id", user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+
+            return GenerateToken(
+                _configuration.AccessTokenSecret,
+                _configuration.Issuer,
+                _configuration.Audience,
+                _configuration.AccessTokenExpirationMinutes,
+                claims);
+        }
+
+        public async Task<string> GetAccessToken(User user)
+        {
+            return GenerateAccessToken(user);
+        }
+    }
+}
+
