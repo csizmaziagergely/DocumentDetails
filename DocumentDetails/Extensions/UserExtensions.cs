34cf634ead693d685ca3516af342f1c8bc0f8716
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 11:38:12 +0200

    add: user extensions

---
+++ DocumentDetails/Extensions/UserExtensions.cs
@@ -0,0 +1,29 @@
+﻿
+using DocumentDetails.DTOs;
+using DocumentDetails.Entities;
+
+namespace BpRobotics.Core.Extensions
+{
+    public static class UserExtensions
+    {
+        public static User ToUserEntity(this UserCreate userCreateData)
+        {
+            return new User
+            {
+                UserName = userCreateData.UserName,
+                HashedPassword = userCreateData.Password,
+                IsActive = true
+            };
+        }
+
+        public static UserView ToUserView(this User user)
+        {
+            return new UserView
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+            };
+        }
+    }
+}
+
