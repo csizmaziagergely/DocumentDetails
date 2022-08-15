085dffdcd014f77d2dabc45853a1aabc3b3a094e
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 18:52:09 +0200

    config: Jwt configure

--- DocumentDetails/Program.cs
+++ DocumentDetails/Program.cs
@@ -1,12 +1,19 @@
 using DocumentDetails;
+using DocumentDetails.DTOs;
 using DocumentDetails.Repositories;
 using DocumentDetails.Services;
 using DocumentDetails.Util;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<AuthenticationConfiguration>(builder.Configuration.GetSection("Authentication"));
+
 
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -28,6 +35,21 @@ builder.Services.AddTransient<DocumentService>();
 builder.Services.AddTransient<UserRepository>();
 builder.Services.AddTransient<UserService>();
 builder.Services.AddTransient<BCryptPasswordHasher>();
+builder.Services.AddTransient<Authenticator>();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Authentication:AccessTokenSecret"])),
+        ValidIssuer = builder.Configuration["Authentication:Audience"],
+        ValidAudience = builder.Configuration["Authentication:Issuer"],
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+    };
+});
 
 var app = builder.Build();
 
