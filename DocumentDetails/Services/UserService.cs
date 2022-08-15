74685f2f4b3e5237ae11faf5f83a4241317a6c22
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-08-15 22:36:53 +0200

    add: user service handling userlogs

--- DocumentDetails/Services/UserService.cs
+++ DocumentDetails/Services/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using BpRobotics.Core.Extensions;
 using DocumentDetails.DTOs;
 using DocumentDetails.Entities;
+using DocumentDetails.Enums;
 using DocumentDetails.Repositories;
 
 namespace DocumentDetails.Services
@@ -8,6 +9,7 @@ namespace DocumentDetails.Services
     public class UserService
     {
         private readonly UserRepository _userRepository;
+        private const int UNSUCCESFULLOGINLIMIT = 5;
         public UserService(UserRepository userRepository)
         {
             _userRepository = userRepository;
@@ -19,8 +21,55 @@ namespace DocumentDetails.Services
             return userEntity.ToUserView();
         }
 
+        public async Task NewUserLog(UserCreateLogin loginRequest, string ipAdress, UserLogType userLogType)
+        {
+            User? userEntity = await _userRepository.GetByUserName(loginRequest.UserName);
+
+
+            UserLog userLog = new UserLog()
+            {
+                UserId = userEntity?.Id,
+                HappenedAt = DateTime.Now,
+                UsernameAttempt = loginRequest.UserName,
+                IPAddress = ipAdress,
+                Type = userLogType
+            };
+            await _userRepository.AddLog(userLog);
+
+            // Deactivate user if limit reached
+            await DeactivateIfLimitReach(userEntity, userLog);
+        }
+
+        private async Task DeactivateIfLimitReach(User? userEntity, UserLog userLog)
+        {
+            if (userEntity != null &&
+                            userEntity.Logs
+                            .Count(l => l.Type == UserLogType.UnsuccessfulLogin) == UNSUCCESFULLOGINLIMIT - 1 &&
+                            userLog.Type == UserLogType.UnsuccessfulLogin)
+            {
+                UserLog newUserLog = new UserLog()
+                {
+                    UserId = userLog.UserId,
+                    HappenedAt = userLog.HappenedAt,
+                    UsernameAttempt = userLog.UsernameAttempt,
+                    IPAddress = userLog.IPAddress,
+                    Type = UserLogType.Deactivation
+                };
+                await _userRepository.AddLog(newUserLog);
+                await DeactivateUser(userEntity);
+            };
+        }
+
+        private async Task DeactivateUser(User user)
+        {
+            user.IsActive = false;
+            await _userRepository.DeactivateUser(user);
+        }
+
         public async Task<UserView> GetById(int userId) => (await _userRepository.GetById(userId)).ToUserView();
         public async Task<User> GetByUserName(string userName) => (await _userRepository.GetByUserName(userName));
+
+
 
     }
 }
